07b5e3ad3e70a052d2de84c129b69605f692971c
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2016-03-25 13:42:06 -0700

    make it spin backwards at the end

--- stuff.js
+++ stuff.js
@@ -16,17 +16,24 @@ var init = function() {
     numFlips++;
     currentImage++;
     
-    if (currentImage >= totalImages) {
-       currentImage = 1; // just keep cycling through the back images
-    }
     var imgToChange;
-    if (currentlyFront) {
-       imgToChange = document.getElementById('backimg');
-       currentlyFront = false;
-    } else {
-       imgToChange = document.getElementById('frontimg');
+    
+    if (currentImage >= totalImages) {
+       numFlips = 0;
+       currentImage = 0;
        currentlyFront = true;
+       imgToChange = document.getElementById('frontimg');
+    } else {
+       
+       if (currentlyFront) {
+          imgToChange = document.getElementById('backimg');
+          currentlyFront = false;
+       } else {
+          imgToChange = document.getElementById('frontimg');
+          currentlyFront = true;
+       }
     }
+    
     imgToChange.src = "side" + currentImage + ".png";
     
     card.style = "transform: rotateY(" + (numFlips * -180) + "deg);";
